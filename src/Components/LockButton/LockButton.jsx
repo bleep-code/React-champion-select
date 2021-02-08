9583f4baf50af5850917c71c45c20e17ea79bdb3
bleep-code <kurekszymon@outlook.com>
2021-02-08 18:03:52 +0100

    added styles for lock-in button

--- src/Components/LockButton/LockButton.jsx
+++ src/Components/LockButton/LockButton.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './LockButton.css';
 class LockButton extends React.Component {
   render() {
-    return <h2>LockButton</h2>;
+    return (
+      <div className="picker__lock-in--wrapper">
+        <div className="picker__lock-in--outer">
+          <div className="picker__lock-in--button"></div>
+          <span className="picker__lock-in--button--text">Lock In</span>
+        </div>
+      </div>
+    );
   }
 }
 
