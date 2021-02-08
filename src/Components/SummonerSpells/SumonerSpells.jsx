9b36c79cce507b9b2d05c7894bbe3aa1e52465d2
bleep-code <kurekszymon@outlook.com>
2021-02-08 19:34:32 +0100

    added placeholder for summoner spells

--- src/Components/SummonerSpells/SumonerSpells.jsx
+++ src/Components/SummonerSpells/SumonerSpells.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import './SummonerSpells.css';
 class SummonerSpells extends React.Component {
   render() {
-    return <p>SummonerSpells</p>;
+    return (
+      <div className="champion-picker__bottom-section--summoner-spells summoner-spells">
+        <div className="summoner-spells--edit-runes"></div>
+        <select className="summoner-spells--choose-runes"></select>
+        <div className="summoner-spells--choose-summoners">
+          <div className="choose-summoners__summoner-spell--left"></div>
+          <div className="choose-summoners__summoner-spell--right"></div>
+        </div>
+        <span className="summoner-spells--delimiter"></span>
+        <div className="summoner-spells--choose-minion"></div>
+        <div className="summoner-spells--choose-emote"></div>
+      </div>
+    );
   }
 }
 
