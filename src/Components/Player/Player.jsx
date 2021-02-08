e680212830a7d2bba3e145e42a3be2e1c66a141e
bleep-code <kurekszymon@outlook.com>
2021-02-08 20:21:33 +0100

    added styles for Player component

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './Player.css';
 class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
-    return <div className="team__players--player player">Player</div>;
+    return (
+      <div className="team__players--player player">
+        <div className="player__picking-sign"></div>
+        <div className="player__summoner-spells">
+          <div className="player_summoner-spells--upper"></div>
+          <div className="player_summoner-spells--lower"></div>
+        </div>
+        <div className="player__image"></div>
+        <div className="player__details">
+          <span className="player__details--is-picking">Status of picking</span>
+          <span className="player__details--position">Bottom</span>
+          <span className="player__details--name">Trundle</span>
+        </div>
+      </div>
+    );
   }
 }
 
