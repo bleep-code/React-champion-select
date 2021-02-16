225ee036502d8add6caf293ea459a6ed342eb935
bleep-code <kurekszymon@outlook.com>
2021-02-16 20:14:55 +0100

    clean code for Player.jsx

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -16,40 +16,46 @@ class Player extends React.Component {
       availableRoles[Math.floor(Math.random() * availableRoles.length)];
   }
 
-  renderBgImage() {
-    const { chosen, locked, isPicking } = this.props;
-    return !!locked
-      ? locked.children[0].src
-      : isPicking && !_.isEmpty(chosen) && chosen?.children[0]?.src;
-  }
-
   render() {
     const { chosen, locked, isPicking, isPickingNext } = this.props;
+
+    const bgImage = !!locked
+      ? locked.children[0].src
+      : isPicking && !_.isEmpty(chosen) && chosen?.children[0]?.src;
+
+    const name = !!locked
+      ? locked.innerText
+      : (isPicking && chosen?.innerText) || this.name;
+
+    const pickingStatus = isPicking
+      ? 'Picking ..'
+      : isPickingNext
+      ? 'Picking next..'
+      : '';
 
     return (
       <div
         className="team__players--player player"
         onClick={this.availableNicknames}
       >
-        <div className="player__picking-sign"></div>
+        <div
+          className="player__picking-sign"
+          style={{ opacity: isPicking ? 1 : 0 }}
+        />
         <div className="player__summoner-spells">
-          <div className="player_summoner-spells--upper"></div>
-          <div className="player_summoner-spells--lower"></div>
+          <div className="player_summoner-spells--upper" />
+          <div className="player_summoner-spells--lower" />
         </div>
         <div
           className="player__image"
           style={{
-            backgroundImage: `url(${this.renderBgImage()})`,
+            backgroundImage: `url(${bgImage})`,
           }}
         ></div>
         <div className="player__details">
-          <span className="player__details--is-picking">
-            {isPicking ? 'Picking ..' : isPickingNext ? 'Picking next..' : ''}
-          </span>
+          <span className="player__details--is-picking">{pickingStatus}</span>
           <span className="player__details--position">{this.role}</span>
-          <span className="player__details--name">
-            {(isPicking && chosen?.innerText) || this.name}
-          </span>
+          <span className="player__details--name">{name}</span>
         </div>
       </div>
     );
