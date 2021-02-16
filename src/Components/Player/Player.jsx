17c5f4688712404cac58d2879f5a6e107a9cb1e9
bleep-code <kurekszymon@outlook.com>
2021-02-16 19:18:53 +0100

    added reactive states for player status

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import './Player.css';
+import {
+  availableNicknames,
+  availableRoles,
+} from '../../Fixtures/fixtures.json';
 
 import _ from 'lodash';
 class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.name =
+      availableNicknames[Math.floor(Math.random() * availableNicknames.length)];
+    this.role =
+      availableRoles[Math.floor(Math.random() * availableRoles.length)];
   }
+
   render() {
-    const { chosen, locked, turn } = this.props;
+    const { chosen, locked, isPicking, isPickingNext } = this.props;
 
     return (
-      <div className="team__players--player player">
+      <div
+        className="team__players--player player"
+        onClick={this.availableNicknames}
+      >
         <div className="player__picking-sign"></div>
         <div className="player__summoner-spells">
           <div className="player_summoner-spells--upper"></div>
@@ -21,14 +33,18 @@ class Player extends React.Component {
           className="player__image"
           style={{
             backgroundImage: `url(${
-              !_.isEmpty(chosen) && chosen?.children[0]?.src
+              isPicking && !_.isEmpty(chosen) && chosen?.children[0]?.src
             })`,
           }}
         ></div>
         <div className="player__details">
-          <span className="player__details--is-picking">Status of picking</span>
-          <span className="player__details--position">Bottom</span>
-          <span className="player__details--name">Trundle</span>
+          <span className="player__details--is-picking">
+            {isPicking ? 'Picking ..' : isPickingNext ? 'Picking next..' : ''}
+          </span>
+          <span className="player__details--position">{this.role}</span>
+          <span className="player__details--name">
+            {(isPicking && chosen?.innerText) || this.name}
+          </span>
         </div>
       </div>
     );
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -8,11 +8,35 @@ class Team extends React.Component {
     return (
       <>
         <div className="team__players">
-          <Player locked={locked} chosen={chosen} turn={turn}></Player>
-          <Player></Player>
-          <Player></Player>
-          <Player></Player>
-          <Player></Player>
+          <Player
+            locked={locked}
+            chosen={chosen}
+            isPicking={turn === 1}
+          ></Player>
+          <Player
+            locked={locked}
+            chosen={chosen}
+            isPicking={turn === 3}
+            isPickingNext={turn === 2}
+          ></Player>
+          <Player
+            locked={locked}
+            chosen={chosen}
+            isPicking={turn === 5}
+            isPickingNext={turn === 4}
+          ></Player>
+          <Player
+            locked={locked}
+            chosen={chosen}
+            isPicking={turn === 7}
+            isPickingNext={turn === 6}
+          ></Player>
+          <Player
+            locked={locked}
+            chosen={chosen}
+            isPicking={turn === 9}
+            isPickingNext={turn === 8}
+          ></Player>
         </div>
       </>
     );
