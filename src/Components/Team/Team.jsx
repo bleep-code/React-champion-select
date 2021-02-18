d67258b2a266f3267317a03b19bd768fad0dffc5
bleep-code <kurekszymon@outlook.com>
2021-02-18 21:49:00 +0100

    working on passing whos picking

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -4,42 +4,43 @@ import './Team.css';
 class Team extends React.Component {
   render() {
     const { chosen, locked, turn, enemy } = this.props;
+    const passedProps = enemy ? '' : 's';
 
     return (
       <>
         <div className="team__players">
           <Player
-            locked={locked?.length >= 1 && locked[0]}
+            locked={locked.length >= 1 && locked[0]}
             chosen={chosen}
-            isPicking={!enemy ? turn === 1 : turn === 2}
+            isPicking={turn === 1}
             enemy={enemy}
           />
           <Player
-            locked={locked?.length >= 3 && locked[2]}
+            locked={locked.length >= 3 && locked[2]}
             chosen={chosen}
-            isPicking={!enemy ? turn === 3 : turn === 4}
-            isPickingNext={!enemy ? turn === 2 : turn === 3}
+            isPicking={turn === 3}
+            isPickingNext={turn === 2}
             enemy={enemy}
           />
           <Player
-            locked={locked?.length >= 5 && locked[4]}
+            locked={locked.length >= 5 && locked[4]}
             chosen={chosen}
-            isPicking={!enemy ? turn === 5 : turn === 6}
-            isPickingNext={!enemy ? turn === 4 : turn === 5}
+            isPicking={turn === 5}
+            isPickingNext={turn === 4}
             enemy={enemy}
           />
           <Player
-            locked={locked?.length >= 7 && locked[5]}
+            locked={locked.length >= 7 && locked[5]}
             chosen={chosen}
-            isPicking={!enemy ? turn === 7 : turn === 8}
-            isPickingNext={!enemy ? turn === 6 : turn === 7}
+            isPicking={turn === 7}
+            isPickingNext={turn === 6}
             enemy={enemy}
           />
           <Player
-            locked={locked?.length >= 9 && locked[7]}
+            locked={locked.length >= 9 && locked[7]}
             chosen={chosen}
-            isPicking={!enemy ? turn === 9 : turn === 10}
-            isPickingNext={!enemy ? turn === 8 : turn === 9}
+            isPicking={turn === 9}
+            isPickingNext={turn === 8}
             enemy={enemy}
           />
         </div>
