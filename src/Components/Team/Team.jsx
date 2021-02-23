5b5ccaf0e9892e9030811f7660fd4df8e7a9ea42
bleep-code <kurekszymon@outlook.com>
2021-02-23 20:01:47 +0100

    removed unnecessary filter method

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -5,26 +5,29 @@ import './Team.css';
 var _ = require('lodash');
 
 class Team extends React.Component {
-  render() {
-    const { chosen, locked, turn, enemy } = this.props;
-    const Players = (isEnemy) =>
-      _.fill(Array(10), undefined)
-        .map((x, i) => {
-          isEnemy = !enemy ? i % 2 === 0 : i % 2 !== 0;
-          return (x = isEnemy && (
-            <Player
-              idx={i}
-              locked={locked?.length >= i + 1 && locked[i]}
-              chosen={chosen}
-              isPicking={turn === i + 1}
-              isPickingNext={turn === i}
-              enemy={enemy}
-            />
-          ));
-        })
-        .filter((x) => x);
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    return <div className="team__players">{Players()}</div>;
+  setPlayers() {
+    const { chosen, locked, turn, enemy } = this.props;
+    return _.fill(Array(10), undefined).map((x, i) => {
+      const isEnemy = !enemy ? i % 2 === 0 : i % 2 !== 0;
+      return (x = isEnemy && (
+        <Player
+          idx={i}
+          locked={locked?.length >= i + 1 && locked[i]}
+          chosen={chosen}
+          isPicking={turn === i + 1}
+          isPickingNext={turn === i}
+          enemy={enemy}
+        />
+      ));
+    });
+  }
+  render() {
+    return <div className="team__players">{this.setPlayers()}</div>;
   }
 }
 
