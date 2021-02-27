94a8e77022efb6cb18077afe2c0469d0ce027a31
bleep-code <kurekszymon@outlook.com>
2021-02-27 15:41:18 +0100

    changed background image, moved back css import in app.jsx to the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 import React from 'react';
 
 import FriendlyTeam from './Components/FriendlyTeam/FriendlyTeam';
@@ -7,6 +5,8 @@ import EnemyTeam from './Components/EnemyTeam/EnemyTeam';
 import ChampionPicker from './Components/ChampionPicker/ChampionPicker';
 import CrashedGame from './Components/CrashedGame/CrashedGame';
 import StartedGame from './Components/StartedGame/StartedGame';
+
+import './App.css';
 
 var _ = require('lodash');
 
--- src/Components/BansList/BansList.jsx
+++ src/Components/BansList/BansList.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
 import './BansList.css';
+
+import React from 'react';
+
 class BansList extends React.Component {
   render() {
     return <div className="team__bans-list bans"></div>;
