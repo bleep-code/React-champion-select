4432d8fd2316c48befcaba58cbce74357bbdc24e
bleep-code <kurekszymon@outlook.com>
2021-03-26 18:49:19 +0100

    added toggle switch component

---
+++ src/Components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,28 @@
+import './ToggleSwitch.css';
+
+import React from 'react';
+
+class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const {leftLabel, rightLabel, isToggled = false, onToggle} = this.props;
+
+    return (
+      <div className="toggle-switch-wrapper">
+        <span className="toggle-switch--label-left">{leftLabel}</span>
+        <label className="toggle-switch">
+          <input type="checkbox" checked={isToggled} onChange={onToggle} />
+          <span className="toggle-switch__slider" />
+        </label>
+        <span className="toggle-switch--label-right">{rightLabel}</span>
+      </div>
+    );
+  }
+}
+
+export default ToggleSwitch;
