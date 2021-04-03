1af5f1c0ce15fdcca197fc3aac5cf9e9be446022
bleep-code <kurekszymon@outlook.com>
2021-04-03 21:59:53 +0200

    hardcoded classic mode, moved state from configurator to a parent component

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -11,11 +11,31 @@ class LandingPage extends React.Component {
 
     this.state = {
       classicMode: false,
+      shouldStart: false,
 
-      configuratorOpen: false,
+      isConfiguratorOpened: false,
+
+      chooseAllPlayers: false,
+
+      customizePlayersCount: false,
+      playersCount: 10,
+
+      banningPhase: true,
+      bansCount: 10,
+
+      customizeMove: false,
+      moveTime: 60,
     };
 
     this.toggleConfigurator = this.toggleConfigurator.bind(this);
+    this.toggleBanningPhase = this.toggleBanningPhase.bind(this);
+    this.toggleChooseAllPlayers = this.toggleChooseAllPlayers.bind(this);
+    this.toggleCustomizeMove = this.toggleCustomizeMove.bind(this);
+    this.toggleCustomizePlayersCount = this.toggleCustomizePlayersCount.bind(this);
+    this.toggleShouldStart = this.toggleShouldStart.bind(this);
+    this.setBansCount = this.setBansCount.bind(this);
+    this.setMoveTime = this.setMoveTime.bind(this);
+    this.setPlayersCount = this.setPlayersCount.bind(this);
   }
 
   renderClassic() {
@@ -23,34 +43,97 @@ class LandingPage extends React.Component {
   }
 
   toggleConfigurator() {
-    this.setState({ configuratorOpen: !this.state.configuratorOpen });
+    this.setState({ isConfiguratorOpened: !this.state.isConfiguratorOpened });
+  }
+
+  toggleShouldStart() {
+    this.setState({ shouldStart: !this.state.shouldStart });
+  }
+
+  toggleChooseAllPlayers() {
+    this.setState({ chooseAllPlayers: !this.state.chooseAllPlayers });
+  }
+
+  toggleBanningPhase() {
+    this.setState({ banningPhase: !this.state.banningPhase });
+  }
+
+  toggleCustomizeMove() {
+    this.setState({ customizeMove: !this.state.customizeMove });
+  }
+
+  toggleCustomizePlayersCount() {
+    this.setState({ customizePlayersCount: !this.state.customizePlayersCount });
+  }
+
+  setBansCount(e) {
+    this.setState({ bansCount: 2 * parseInt(e.target.value) });
+  }
+
+  setMoveTime(e) {
+    this.setState({ moveTime: parseInt(e.target.value) });
+  }
+
+  setPlayersCount(e) {
+    this.setState({ playersCount: 2 * parseInt(e.target.value) });
   }
 
   render() {
-    const { classicMode, configuratorOpen } = this.state;
+    const {
+      classicMode,
+      isConfiguratorOpened,
+      shouldStart,
+      chooseAllPlayers,
+      banningPhase,
+      bansCount,
+      customizeMove,
+      moveTime,
+      customizePlayersCount,
+      playersCount,
+    } = this.state;
 
     if (classicMode) {
-      return <ChampionSelect />;
+      return (
+        <ChampionSelect
+          banningPhase={true}
+          bansCount={10}
+          playersCount={10}
+          moveTime={60}
+          chooseAllPlayers={true}
+        />
+      );
     }
 
     return (
       <div className="start-view">
-        {configuratorOpen && (
-          <Configurator toggleConfigurator={this.toggleConfigurator} />
+        {isConfiguratorOpened && (
+          <Configurator
+            toggleConfigurator={this.toggleConfigurator}
+            shouldStart={shouldStart}
+            toggleShouldStart={this.toggleShouldStart}
+            chooseAllPlayers={chooseAllPlayers}
+            toggleChooseAllPlayers={this.toggleChooseAllPlayers}
+            banningPhase={banningPhase}
+            toggleBanningPhase={this.toggleBanningPhase}
+            bansCount={bansCount}
+            setBansCount={this.setBansCount}
+            customizeMove={customizeMove}
+            toggleCustomizeMove={this.toggleCustomizeMove}
+            moveTime={moveTime}
+            setMoveTime={this.setMoveTime}
+            customizePlayersCount={customizePlayersCount}
+            toggleCustomizePlayersCount={this.toggleCustomizePlayersCount}
+            playersCount={playersCount}
+            setPlayersCount={this.setPlayersCount}
+          />
         )}
-        {!configuratorOpen && (
+        {!isConfiguratorOpened && (
           <>
-            <div
-              className="start-view__classic-mode"
-              onClick={() => this.renderClassic()}
-            >
+            <div className="start-view__classic-mode" onClick={() => this.renderClassic()}>
               <i className="far fa-compass" />
               <span>Explore</span>
             </div>
-            <div
-              className="start-view__custom-mode"
-              onClick={this.toggleConfigurator}
-            >
+            <div className="start-view__custom-mode" onClick={this.toggleConfigurator}>
               <i className="far fa-folder" />
               <span>Customize</span>
             </div>
