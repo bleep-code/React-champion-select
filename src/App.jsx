25443815454e22730e6e8b15e938f3aa72aa7461
bleep-code <kurekszymon@outlook.com>
2021-05-06 19:58:39 +0200

    resolved merge conflict for app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import './App.css';
 
-<<<<<<< HEAD
-import LandingPage from './Components/GameScreens/LandingPage/LandingPage';
-=======
 import React from 'react';
->>>>>>> master
 
 import LandingPage from './Components/Screens/LandingPage/LandingPage';
 
